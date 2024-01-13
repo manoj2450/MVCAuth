4fcba20e6808457c06f7cb8a927e4e1d9fec055a
Manoj2450 <manoj2450@gmail.com>
2024-01-13 13:46:51 +0530

    add initial files

---
+++ Authorasation/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Authorasation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Authorasation.Controllers
+{
+    [AllowAnonymous]//to exclude from autorazation 
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            EmployeeEntities3 emp = new EmployeeEntities3();
+            var count = emp.Users.Where(x => x.UserName == user.UserName && x.Password == user.Password).Count();
+            if (count == 0)
+            {
+                ViewBag.msg = "User is Invalid !!!";
+                return View();
+            }
+            else
+            {
+                FormsAuthentication.SetAuthCookie(user.UserName, false);
+                return RedirectToAction("Index", "Home");
+               
+            }
+            
+
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("login", "Account");
+        }
+    }
+}
---
+++ Authorasation/Controllers/tbl_EmployeeController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Authorasation.Models;
+
+namespace Authorasation.Controllers
+{
+    public class tbl_EmployeeController : Controller
+    {
+        private EmployeeEntities3 db = new EmployeeEntities3();
+
+        // GET: tbl_Employee
+        [Authorize(Roles = "V,A")]
+        public ActionResult Index()
+        {
+            return View(db.tbl_Employee.ToList());
+        }
+
+        // GET: tbl_Employee/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // GET: tbl_Employee/Create
+        [Authorize(Roles = "A")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tbl_Employee/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Eid,EName,Salary,DOB,Gender,Depid")] tbl_Employee tbl_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbl_Employee.Add(tbl_Employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tbl_Employee);
+        }
+
+        // GET: tbl_Employee/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // POST: tbl_Employee/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Eid,EName,Salary,DOB,Gender,Depid")] tbl_Employee tbl_Employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbl_Employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tbl_Employee);
+        }
+
+        // GET: tbl_Employee/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            if (tbl_Employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbl_Employee);
+        }
+
+        // POST: tbl_Employee/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbl_Employee tbl_Employee = db.tbl_Employee.Find(id);
+            db.tbl_Employee.Remove(tbl_Employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
